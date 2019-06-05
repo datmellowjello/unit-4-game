b6ccb7d453c7464e77c8bc6c7d40f582900de99d
davidbridges123 <david.bridges@live.com>
2019-06-05 12:00:02 -0600

    adding variables and pseudo code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,21 @@ var randNum = [];
 // need to make array for random crystal numbers
 var crystalNum = [];
 // need to make variables for wins, loses, 
+var wins = 0;
+var loses = 0;
+// need varibales for each crystal
+var c1;
+var c2;
+var c3;
+var c4;
 
+
+// need function to pick random crystal numbers 
+
+// need function to pick random computer number to match 
+
+
+// need to make reset function
 
 
 
