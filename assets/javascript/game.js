b038257a95cf1ba36e7b32d68e85c2dff897e988
davidbridges123 <david.bridges@live.com>
2019-06-05 11:50:33 -0600

    adding js

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+
+// need to make array for random computer number 
+
+// need to make array for random crystal numbers
+
+// need to make variables for wins, loses, 
+
+
+
+
+
+});
