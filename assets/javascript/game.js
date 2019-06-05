ea5e079f625b0af0ddab355ccfa8bfafa5c4c3f2
davidbridges123 <david.bridges@live.com>
2019-06-05 12:52:45 -0600

    added random number to dom

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,10 +4,9 @@ $(document).ready(function () {
 
 
 // need to make array for random computer number (19 - 120)
-var randNum = [];
-for (var i = 19; i < 121; i++) {
-    randNum.push(i);
-}
+var randNum = Math.floor(Math.random()*102+19)
+
+$("#randomNumber").text(randNum);
 // need to make array for random crystal numbers ( 1 -12)
 var crystalNum = [];
 for (var c = 1; c < 13; c++) {
@@ -20,41 +19,17 @@ var loses = 0;
 
 
 // array for storing the random numbers computer picked 
-var randomNumber = []; // number to match
-var crystalNumbers = []; //array for number randomly chosen
 
 // need varibales for each crystal
-var c1;
-var c2;
-var c3;
-var c4;
+
 
 
 
 // need function to pick random crystal numbers 
 
-function pickRandomNumber(arr) {
 
-    var x = arr[Math.floor(Math.random() * arr.length)];
-    randNumber = x;
-    $("#chosenNum").html(randNumber);
-
-    console.log("random number: " + randNumber);
-
-} 
 
 // pick random numbers for crystals
-
-function pickRandomCrystals(arr) {
-
-    for (var y = 0; y < 4; y++){
-
-        var a = arr[Math.floor(Math.random() * arr.length)];
-
-        crystalNumbers.push(a);
-    }
-
-
 
 // need to make reset function
 
